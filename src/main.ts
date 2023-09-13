fc02d500dfc13af35a79bcdb4c25a61b5555ae9c
Hardik Thakkar <hardik.thakkar@healthdirect.org.au>
2023-09-13 17:02:26 +1000

    port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(443);
+  await app.listen(80);
+  console.log('Listening on port 80');
 }
 bootstrap();
